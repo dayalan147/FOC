4022f7eed7874749978f6cdf63e0a5d12e457e8c
dayalan147 <122363911+dayalan147@users.noreply.github.com>
2023-01-10 17:41:43 +0530

    Add files via upload

---
+++ positive or negative.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the value of n");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("positive integer");		
+	}
+	else
+	{
+		printf("negative integer");
+	}
+	return 0;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact 
